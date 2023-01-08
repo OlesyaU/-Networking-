1015e8f7d5ef8fb8a234a4de5c655f2a69cee00e
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-01-08 10:09:52 +0300

    чуть подчистила проект и прокомментировала подсказки пароля и прочих алертов (эти лишь для теста опционала приложения и упрощения)

--- Navigation/Coordinators/FeedCoordinator/FeedCoordinator.swift
+++ Navigation/Coordinators/FeedCoordinator/FeedCoordinator.swift
@@ -26,6 +26,7 @@ final class FeedCoordinator: Coordinator {
                 nvc.pushViewController(postVC, animated: true)
             } else {
                 let alert = UIAlertController(title: "Incorrect or NO data ", message: "You didn't write right password. Change this please to continue. Correct password is \"Password\"" , preferredStyle: .alert)
+//                 тут есть подсказки, но безусловно, для экономии времени теста опционала- в реальном проложении сообщения ОБЯЗАНЫ быть другими- без содержания пароля или каких-либо данных пользователя
                 let act = UIAlertAction(title: "Ok", style: .cancel)
                 alert.addAction(act)
                 controller.present(alert, animated: true)

b5424300ff779ff3f122b419d36a075c72c54bbd
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-01-08 12:43:11 +0300

    ✅Создайте сетевой сервис, типа struct NetworkManager или struct NetworkService. В NetworkService пока будет только 1 пустой метод static func request() {}. Позже он будет запускать URLSessionDataTask для определенного URL. ✅Объявите enum AppConfiguration, в нем будут 3 case с ассоциированными значениями типа URL или String, на ваше усмотрение.

---
+++ Navigation/AppConfiguration.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfiguration.swift
+//  Navigation
+//
+//  Created by Олеся on 08.01.2023.
+//
+
+import Foundation
+
+enum AppConfiguration {
+    case config1(URL)
+    case config2(URL)
+    case config3(URL)
+}
+//case 1(URL(string: "https://swapi.dev/api/people/8"))
+//case 2(URL(string: "https://swapi.dev/api/starships/3"))
+//case 3(URL(string: "https://swapi.dev/api/planets/5"))
---
+++ Navigation/NetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkService.swift
+//  Navigation
+//
+//  Created by Олеся on 08.01.2023.
+//
+
+import Foundation
+
+struct NetworkService {
+    
+    static func request () {
+        
+    }
+}

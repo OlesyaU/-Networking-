e27c83c31b17bf706912334a92847157be309d52
Olesya <53311515+OlesyaU@users.noreply.github.com>
2023-01-27 12:32:16 +0300

    ✅Задание 1 URL 1 - /{любой номер на ваше усмотрение}
    Создайте модель данных на базе JSON
    
    Запустите URLSessionDataTask
    
    С помощью JSONSerialization инициализируйте объект в do try catch блоке. Используйте метод jsonObject(with data: Data, options opt: JSONSerialization.ReadingOptions = []).
    
    В дополнительно созданной UILabel на InfoViewController отобразите значение поля title

--- Navigation/Info/Controller/InfoViewController.swift
+++ Navigation/Info/Controller/InfoViewController.swift
@@ -11,18 +11,18 @@ class InfoViewController: UIViewController {
     
     lazy var goToAlertButton: CustomButton = {
         let button = CustomButton(title: "Push on me", background: .systemCyan, titleColor: .white)
-        button.frame = CGRect(origin: CGPoint(x: 100, y: 150), size: CGSize(width: 150, height: 30))
+        button.frame = CGRect(origin: CGPoint(x: 100, y: 50), size: CGSize(width: 150, height: 30))
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-//        button.translatesAutoresizingMaskIntoConstraints = false
+        //        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(buttonTap(_sender:)), for: .touchUpInside)
         return button
     }()
     
     lazy var jsonTitleLabel: UILabel = {
-        let label = UILabel(frame: CGRect(origin: CGPoint(x: 20, y: 200), size: CGSize(width: 250, height: 150)))
+        let label = UILabel(frame: CGRect(origin: CGPoint(x: 20, y: 100), size: CGSize(width: 300, height: 100)))
         label.numberOfLines = 0
-        label.backgroundColor = .lightGray
-        
+        label.backgroundColor = .white
+        label.textColor = .black
         return label
         
     }()
@@ -33,17 +33,20 @@ class InfoViewController: UIViewController {
     }
     
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ModelInfo().getJsonInfo({ [weak self] text in
+            DispatchQueue.main.async {
+                self?.jsonTitleLabel.text = text
+            }
+        })
+    }
     
     private func layout(){
         view.backgroundColor = .systemYellow
         view.addSubview(goToAlertButton)
         view.addSubview(jsonTitleLabel)
-        
-        
-        
     }
-    
     
     @objc private func buttonTap(_sender: UIButton) {
         let alert = UIAlertController(title: "OOPS", message: "Something wrong!Run!", preferredStyle: .actionSheet)
--- Navigation/Info/Model/InfoJSONModel.swift
+++ Navigation/Info/Model/InfoJSONModel.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 /*
-"userId": 1,
-"id": 1,
-"title": "delectus aut autem",
-"completed": false
+ "userId": 1,
+ "id": 1,
+ "title": "delectus aut autem",
+ "completed": false
  */
 struct JsonInfo {
     var userId: Int
@@ -19,52 +19,54 @@ struct JsonInfo {
     var title: String
     var completed: Bool
 }
+struct Answer {
+    var json: [JsonInfo]
+}
+
 struct ModelInfo {
     
-    func getJsonInfo(_ completin: ((String)-> Void)?) {
+    func getJsonInfo(_ completion: ((_ textTitle:String?)-> Void)?) {
         let session = URLSession(configuration: .default)
         guard let URL = URL(string: "https://jsonplaceholder.typicode.com/todos/") else {return}
         let task = session.dataTask(with: URL) { data, responce, error in
             if let error {
                 print(error.localizedDescription)
+                completion?(nil)
+                return
             }
             let statusCode = (responce as? HTTPURLResponse)?.statusCode
             
             if statusCode != 200 {
                 print("Status code != 200. StatusCode = \(String(describing: statusCode))")
+                completion?(nil)
                 return
-                
             }
             
-           guard let data  else {
+            guard let data  else {
                 print("Data = nil")
-               return
+                completion?(nil)
+                return
             }
             
             do {
-                guard let answer = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-                    print ("Error parcing json answer")
+                guard let answer = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [[String: Any]] else {
+                    print("error parcing JSON")
+                    completion?(nil)
                     return
                 }
-                let text = answer["title"]
-//                guard let result =  answer["result"] as? [String: Any] else { print ("Error parcing json result ")
-//                    return
-//                }
-//
-//                var resultArray: [String] = []
-//
-//                for item in result {
-//                    if let joketext = item["value"] as? String {
-//                        resultArray.append(joketext)
-//                    }
-//                }
-       
+//                тут берем тайтл у рандомного JSON'a - это можно изменить
+                guard let text = answer.randomElement()!["title"] as? String else {
+                    completion?(nil)
+                    return
+                }
                 
+                completion?(text)
             }
             catch {
                 print(error)
+                completion?(nil)
             }
         }
+        task.resume()
     }
-    
 }
